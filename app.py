7ca3c8892193374ef70fed3a4a4ef6210004c2b7
Charishma Tata <153920644+charishma101@users.noreply.github.com>
2024-05-05 23:07:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def main():
     st.write("Press 'q' to quit")
 
     # Open the webcam
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0, cv2.CAP_V4L)
 
     # Continuously capture frames
     while True:
