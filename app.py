0e3eb7d562735318f4333d1c4da091d6429e407f
Charishma Tata <153920644+charishma101@users.noreply.github.com>
2024-05-06 15:10:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ import cv2
 import streamlit as st
 import numpy as np
 
+device_path = "USB\VID_2BDF&PID_0284&MI_00\6&5D2DB91&0&0000"
+
 # For webcam input:
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(device_path)
 
 st.title("Obj Det webcam")
 frame_placeholder = st.empty()
