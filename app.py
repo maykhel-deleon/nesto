9648f2a7bc11736f17031dc5c7aa0eb7140f55ba
Charishma Tata <153920644+charishma101@users.noreply.github.com>
2024-05-06 12:13:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,146 +1,90 @@
 import cv2
-import numpy as np
+import mediapipe as mp
+
+import tempfile
 import streamlit as st
-import os
-import io
-import glob
-import random
-from tensorflow.lite.python.interpreter import Interpreter
-import streamlit_webrtc as webrtc
-import matplotlib.pyplot as plt
-from PIL import Image
-from streamlit_webrtc import webrtc_streamer, RTCConfiguration
 
-# Define paths
-PATH_TO_MODEL = './detect.tflite'
-PATH_TO_LABELS = './labelmap.txt'
 
-# Define the tflite_detect_images function
-def tflite_detect_images(image, modelpath, lblpath, min_conf=0.5, txt_only=False):
-    # Load the label map into memory
-    with open(lblpath, 'r') as f:
-        labels = [line.strip() for line in f.readlines()]
 
-    # Load the Tensorflow Lite model into memory
-    interpreter = Interpreter(model_path=modelpath)
-    interpreter.allocate_tensors()
 
-    # Get model details
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
-    height = input_details[0]['shape'][1]
-    width = input_details[0]['shape'][2]
 
-    # Convert the uploaded image to numpy array
-    
 
-    # Convert the uploaded image to a PIL Image
-    pil_image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-    # Convert the PIL Image to a NumPy array
-    image = np.array(pil_image)
 
-    # Check the data type of the image
-    #st.write(image.dtype)
-
-    # Preprocess the image
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    imH, imW, _ = image.shape
-    image_resized = cv2.resize(image_rgb, (width, height))
-    input_data = np.expand_dims(image_resized, axis=0)
-
-    # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
-    float_input = (input_details[0]['dtype'] == np.float32)
-    if float_input:
-        input_mean = 127.5
-        input_std = 127.5
-        input_data = (np.float32(input_data) - input_mean) / input_std
-
-    # Perform object detection by running the model with the image as input
-    interpreter.set_tensor(input_details[0]['index'], input_data)
-    interpreter.invoke()
-
-    # Retrieve detection results
-    boxes = interpreter.get_tensor(output_details[1]['index'])[0] # Bounding box coordinates of detected objects
-    classes = interpreter.get_tensor(output_details[3]['index'])[0] # Class index of detected objects
-    scores = interpreter.get_tensor(output_details[0]['index'])[0] # Confidence of detected objects
-
-    detections = []
-
-    # Loop over all detections and draw detection box if confidence is above minimum threshold
-    for i in range(len(scores)):
-        if ((scores[i] > min_conf) and (scores[i] <= 1.0)):
-            # Get bounding box coordinates and draw box
-            ymin = int(max(1,(boxes[i][0] * imH)))
-            xmin = int(max(1,(boxes[i][1] * imW)))
-            ymax = int(min(imH,(boxes[i][2] * imH)))
-            xmax = int(min(imW,(boxes[i][3] * imW)))
-            cv2.rectangle(image, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
-
-            # Draw label
-            object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
-            label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
-            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
-            label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
-            cv2.rectangle(image, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
-            cv2.putText(image, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
-
-            detections.append([object_name, scores[i], xmin, ymin, xmax, ymax])
-
-    # Display or save the image with detections
-    if txt_only == False:
-        image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-        st.image(image, caption="Object Detection Result", use_column_width=True)
-        #plt.figure(figsize=(12, 16))
-        #plt.imshow(image)
-        #plt.axis('off')
-        #st.pyplot()
-        
-    
-    return 
-    
-def scale_resolution(frame):
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = cv2.resize(frame, (width, height))
-        return frame
-    
-# Main Streamlit app
 def main():
-    st.title('Object Detection using Webcam')
-    #st.write("Press 'q' to quit")
-    picture = st.camera_input("Take a picture")
 
-    if picture:
-        st.image(picture)
+    #title 
+    st.title('Face Detection App')
 
-    # Open the webcam
-    cap = cv2.VideoCapture(1)
+    #sidebar title
+    st.sidebar.title('Face Detection App')
 
-    # Continuously capture frames
-    while True:
-        ret, frame = cap.read()
+    st.sidebar.subheader('Parameters')
+    #creating a button for webcam
+    use_webcam = st.sidebar.button('Use Webcam')
+    #creating a slider for detection confidence 
+    detection_confidence = st.sidebar.slider('Min Detection Confidence', min_value =0.0,max_value = 1.0,value = 0.5)
+    
+    #model selection 
+    model_selection = st.sidebar.selectbox('Model Selection',options=[0,1,2])
+    st.markdown(' ## Output')
+    stframe = st.empty()
+    
+    #file uploader
+    video_file_buffer = st.sidebar.file_uploader("Upload a video", type=[ "mp4", "mov",'avi','asf', 'm4v' ])
 
-        # Check if the capture was successful
-        if not ret:
-            st.write("Failed to capture image")
-            break
+    
+    #temporary file name 
+    tfflie = tempfile.NamedTemporaryFile(delete=False)
 
-        # Convert the OpenCV image (BGR) to PIL image (RGB)
-        pil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    if not video_file_buffer:
 
-        # Perform object detection on pil_image using tflite_detect_images function
-        tflite_detect_images(pil_image, PATH_TO_MODEL, PATH_TO_LABELS, min_conf=0.5, txt_only=False)
+        if use_webcam:
+            vid = cv2.VideoCapture(0)
+       
+    else:
+        tfflie.write(video_file_buffer.read())
+        vid = cv2.VideoCapture(tfflie.name)
 
-        # Display the frame with Streamlit
-        st.image(frame, channels="BGR")
+    #values 
+    width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(vid.get(cv2.CAP_PROP_FPS))
+    #codec = cv2.VideoWriter_fourcc(*FLAGS.output_format)
+    codec = cv2.VideoWriter_fourcc('V','P','0','9')
+    out = cv2.VideoWriter('output1.webm', codec, fps, (width, height))
 
-        # Check for 'q' key press to exit
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
 
-    # Release the webcam and close the Streamlit app
-    cap.release()
-    cv2.destroyAllWindows()
+    st.sidebar.text('Input Video')
+    st.sidebar.video(tfflie.name)
+
+    # drawing_spec = mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+
+
+    with mp_face_detection.FaceDetection(
+    model_selection=model_selection, min_detection_confidence=detection_confidence) as face_detection:
+        
+        while vid.isOpened():
+
+            ret, image = vid.read()
+
+            if not ret:
+                break
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = face_detection.process(image)
+
+            if results.detections:
+                for detection in results.detections:
+                    mp_drawing.draw_detection(image, detection)
+            stframe.image(image,use_column_width=True)
+
+        vid.release()
+        out.release()
+        cv2.destroyAllWindows()
+
+    st.success('Video is Processed')
+    st.stop()
 
 if __name__ == '__main__':
     main()
