bb688ac6a31d9649ebed0e8049ddb0a26a93ba8f
Charishma Tata <153920644+charishma101@users.noreply.github.com>
2024-05-06 15:15:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 import cv2
 import streamlit as st
 import numpy as np
+from streamlit_webrtc import webrtc_streamer
 
-device_path = "USB\VID_2BDF&PID_0284&MI_00\6&5D2DB91&0&0000"
+webrtc_streamer(key="sample")
 
-# For webcam input:
-cap = cv2.VideoCapture(device_path)
-
-st.title("Obj Det webcam")
-frame_placeholder = st.empty()
-st.write(cap.isOpened())
